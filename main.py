0614ba28a8d3bcf8baec0194d710376464599afd
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2025-02-23 15:17:06 -0300

    feat: add outliers img generation in analysis

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from data_analysis.detect_outliers import detect_outliers
 from data_export.export_to_csv import export_to_csv
 from data_preprocessing.load_data import load_data
 from data_preprocessing.analyse_initial_data import analyze_initial_data
 from data_preprocessing.transform_data import transform_data
-
 
 def main():
     FILE_PATH = 'data_test.csv'
     FILE_PATH_INPUT_DATA = 'data_input.csv'
     FILE_PATH_FORECAST_DATA = 'data_forecast.csv'
 
+    FILE_PATH_IMGS_OUTLIERS = 'imgs/outliers'
 
     # Carregar dados
     df_loaded = load_data(FILE_PATH)
@@ -16,11 +17,13 @@ def main():
         return
 
     # Analisar dados inicias (exibir quais os nomes dos atributos e tipos de dados)
-    analyze_initial_data(df_loaded)
+    # analyze_initial_data(df_loaded)
 
     # Realizar a transposição dos valores de acordo com o tempo, separando em dados de entrada e dados previstos
     df_input, df_forecast = transform_data(df_loaded)
 
+    detect_outliers(df_input, FILE_PATH_IMGS_OUTLIERS)
+    
     export_to_csv(df_input, FILE_PATH_INPUT_DATA)
     export_to_csv(df_forecast, FILE_PATH_FORECAST_DATA)
 
